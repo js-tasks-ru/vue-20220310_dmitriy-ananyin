6ea27d2008e99eb3225fe14d26afe70bf1451b0a
Dmitriy Ananyin <dmitriy.ananyin.web@gmail.com>
2022-03-17 01:39:24 +0630

    Solve VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      numA: 0,
+      numB: 0,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    handleResult() {
+      let result;
+      switch (this.operator) {
+        case 'sum':
+          result = this.numA + this.numB;
+          break;
+        case 'subtract':
+          result = this.numA - this.numB;
+          break;
+        case 'multiply':
+          result = this.numA * this.numB;
+          break;
+        case 'divide':
+          result = this.numA / this.numB;
+          break;
+      }
+      return result;
+    },
+  },
+}).mount('#app');
