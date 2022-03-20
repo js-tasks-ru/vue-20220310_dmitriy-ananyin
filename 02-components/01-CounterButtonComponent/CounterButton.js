c6d186968c9f86d0c73b94cb8381e9b6b32be89c
Dmitriy Ananyin <dmitriy.ananyin.web@gmail.com>
2022-03-21 01:03:59 +0630

    Solve CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,10 +3,8 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
 
-  // Компонент должен иметь входной параметр
+  props: ['count'],
+  emits: ['update'],
 
-  // Шаблон лучше держать максимально простым, а логику выносить в методы
-
-  // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="$emit('update', 1)">{{ count }}</button>`,
 });
