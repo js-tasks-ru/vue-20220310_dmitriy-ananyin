519ce7c080005478dea027eaf4baf7cfde394b60
Dmitriy Ananyin <dmitriy.ananyin.web@gmail.com>
2022-03-21 00:12:51 +0630

    Solve MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+createApp({
+  name: 'App',
+  data() {
+    return {
+      picked: 1,
+      title: '',
+    };
+  },
+  watch: {
+    picked: {
+      async handler(n = 1) {
+        const data = await this.fetchMeetupById(n);
+        this.title = data.title;
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    async fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
-    }
-  });
-}
+    },
+  },
+}).mount('#app');
 
 // Требуется создать Vue приложение
