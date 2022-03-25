5379d60476c9f2422b350d873cb8021de7ea5463
Dmitriy Ananyin <dmitriy.ananyin.web@gmail.com>
2022-03-25 23:15:59 +0630

    Update 03-sfc/01-VBindCover 2

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -32,7 +32,7 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(checkImage);
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(imageHandler);
   display: flex;
   flex-direction: column;
   align-items: center;
